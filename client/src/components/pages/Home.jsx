2c5ee45b80a85bdbd9422fd5a5972f0e6e79d070
hap196 <haileyrpan@gmail.com>
2025-01-16 17:53:35 -0500

    add branch functionality

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,14 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 import "../../utilities.css";
 import "./Home.css";
 import { UserContext } from "../App";
 import background from "/background.png";
-import nobranch from "/nobranch.png";
+import noBranch from "/noBranch.png";
+import oneBranch from "/oneBranch.png";
+import twoBranch from "/twoBranch.png";
+import threeBranch from "/threeBranch.png";
+import fourBranch from "/fourBranch.png";
+import fiveBranch from "/fiveBranch.png";
+import sixBranch from "/sixBranch.png";
+import CustomButton from "../modules/CustomButton";
+
+const branchImages = [noBranch, oneBranch, twoBranch, threeBranch, fourBranch, fiveBranch, sixBranch];
 
 const Home = () => {
   const { userId, handleLogin, handleLogout } = useContext(UserContext);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const handleAddBranch = () => {
+    setCurrentImageIndex((prevIndex) => {
+      // do not go past the last image
+      if (prevIndex < branchImages.length - 1) {
+        return prevIndex + 1;
+      }
+      return prevIndex;
+    });
+  };
+
   return (
     <>
       {userId ? (
@@ -24,7 +45,8 @@ const Home = () => {
         <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
       )}
       <img className="background-image" src={background} alt="Background" />
-      <img className="tree-image" src={nobranch} alt="Tree with no branches" />
+      <img className="tree-image" src={branchImages[currentImageIndex]} alt="Tree with branches" />
+      <CustomButton text="Add Branch" onClick={handleAddBranch} className="add-branch-button"/>
     </>
   );
 };
