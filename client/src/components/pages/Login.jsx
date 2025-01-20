3c1d1cede4950790da763758598a637f78b64710
Jensen Coonradt <therobotwhisperer31@gmail.com>
2025-01-19 23:35:18 -0800

    Improved home page styling with sign

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext } from "react";
 import { GoogleLogin } from "@react-oauth/google";
-import { UserContext } from '../App'
+import { UserContext } from "../App";
 import "./Login.css";
 
 const Login = () => {
@@ -10,14 +10,21 @@ const Login = () => {
     <div className="Login-container">
       {/* <Navbar /> */}
       <img src="/loginBackground.png" alt="Background" className="login-background" />
+
+      <div className="information-sign">
+        <img src="/woodenSign.png" alt="Info Sign" className="information-sign" />
+        <h1>Welcome to Skills Tree!</h1>
+        <h2>
+          Grow your potential, branch by branch. Discover opportunities, nurture your talents, and
+          watch your skills flourish. With our Skills Tree, every goal is a seed, and every
+          achievement a leaf in your journey to success.
+        </h2>
+      </div>
       <div className="Login-googleButton">
-        <GoogleLogin 
-          onSuccess={handleLogin} 
-          onError={(err) => console.log(err)} 
-        />
+        <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
