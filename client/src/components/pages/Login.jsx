c9e4e4799353e08c2853caa4410b5b638099df25
Jensen Coonradt <therobotwhisperer31@gmail.com>
2025-01-24 17:49:14 -0600

    Updated home screen

--- client/src/components/pages/Login.jsx
+++ client/src/components/pages/Login.jsx
@@ -3,6 +3,10 @@ import { GoogleLogin } from "@react-oauth/google";
 import { UserContext } from "../App";
 import "./Login.css";
 import loginBackground from "../../assets/loginBackground.png";
+import logInTree from "../../assets/logInTree.png";
+import racoon from "../../assets/racoon.gif";
+import owl from "../../assets/owl.gif";
+import WoodenSign from "../../assets/woodenSign.png";
 
 const Login = () => {
   const { handleLogin } = useContext(UserContext);
@@ -11,17 +15,26 @@ const Login = () => {
     <div className="Login-container">
       {/* <Navbar /> */}
       <img src={loginBackground} alt="Background" className="login-background" />
-
-      <div className="information-sign">
-        <img src="/woodenSign.png" alt="Info Sign" className="information-sign" />
+      <img src={logInTree} alt="Background" className="login-tree" />
+      <img src={racoon} alt="Cute racoon" className="login-racoon" />
+      <img src={owl} alt="Cute owl" className="login-owl" />
+      <div className="information-signL">
+        <img src={WoodenSign} alt="Info Sign" className="information-signL" />
         <h1>Welcome to Skills Tree!</h1>
         <h2>
-          Grow your potential, branch by branch. Discover opportunities, nurture your talents, and
-          watch your skills flourish. With our Skills Tree, every goal is a seed, and every
-          achievement a leaf in your journey to success.
+          With our Skills Tree, every goal is a seed, and every achievement a leaf in your journey
+          to success
         </h2>
       </div>
-      <div className="Login-googleButton">
+
+      <div
+        style={{
+          position: "fixed",
+          bottom: "200px",
+          left: "50%",
+          transform: "translateX(-50%)",
+        }}
+      >
         <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
       </div>
     </div>
